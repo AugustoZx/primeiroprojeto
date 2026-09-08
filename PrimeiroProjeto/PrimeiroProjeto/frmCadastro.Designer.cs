f622d69b7c803914db8d9fcca767b49cea65ff2f
Augusto Oliveira <augustooliiveira@hotmail.com>
2026-09-08 15:26:20 -0300

    update

--- PrimeiroProjeto/PrimeiroProjeto/frmCadastro.Designer.cs
+++ PrimeiroProjeto/PrimeiroProjeto/frmCadastro.Designer.cs
@@ -42,27 +42,30 @@
             // lblNome
             // 
             this.lblNome.AutoSize = true;
-            this.lblNome.Location = new System.Drawing.Point(36, 78);
+            this.lblNome.Location = new System.Drawing.Point(27, 63);
+            this.lblNome.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblNome.Name = "lblNome";
-            this.lblNome.Size = new System.Drawing.Size(44, 16);
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
             this.lblNome.TabIndex = 0;
             this.lblNome.Text = "Nome";
             // 
             // lblEmail
             // 
             this.lblEmail.AutoSize = true;
-            this.lblEmail.Location = new System.Drawing.Point(36, 128);
+            this.lblEmail.Location = new System.Drawing.Point(27, 104);
+            this.lblEmail.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblEmail.Name = "lblEmail";
-            this.lblEmail.Size = new System.Drawing.Size(41, 16);
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
             this.lblEmail.TabIndex = 1;
             this.lblEmail.Text = "Email";
             // 
             // lblSenha
             // 
             this.lblSenha.AutoSize = true;
-            this.lblSenha.Location = new System.Drawing.Point(36, 178);
+            this.lblSenha.Location = new System.Drawing.Point(27, 145);
+            this.lblSenha.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblSenha.Name = "lblSenha";
-            this.lblSenha.Size = new System.Drawing.Size(46, 16);
+            this.lblSenha.Size = new System.Drawing.Size(38, 13);
             this.lblSenha.TabIndex = 2;
             this.lblSenha.Text = "Senha";
             // 
@@ -75,16 +78,18 @@
             this.pnlCadastro.Controls.Add(this.lblNome);
             this.pnlCadastro.Controls.Add(this.lblSenha);
             this.pnlCadastro.Controls.Add(this.lblEmail);
-            this.pnlCadastro.Location = new System.Drawing.Point(160, 12);
+            this.pnlCadastro.Location = new System.Drawing.Point(136, 10);
+            this.pnlCadastro.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pnlCadastro.Name = "pnlCadastro";
-            this.pnlCadastro.Size = new System.Drawing.Size(439, 426);
+            this.pnlCadastro.Size = new System.Drawing.Size(329, 346);
             this.pnlCadastro.TabIndex = 3;
             // 
             // btnCadastrar
             // 
-            this.btnCadastrar.Location = new System.Drawing.Point(166, 262);
+            this.btnCadastrar.Location = new System.Drawing.Point(124, 213);
+            this.btnCadastrar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCadastrar.Name = "btnCadastrar";
-            this.btnCadastrar.Size = new System.Drawing.Size(106, 32);
+            this.btnCadastrar.Size = new System.Drawing.Size(80, 26);
             this.btnCadastrar.TabIndex = 6;
             this.btnCadastrar.Text = "Cadastrar";
             this.btnCadastrar.UseVisualStyleBackColor = true;
@@ -92,33 +97,41 @@
             // 
             // txtSenha
             // 
-            this.txtSenha.Location = new System.Drawing.Point(86, 175);
+            this.txtSenha.Location = new System.Drawing.Point(64, 142);
+            this.txtSenha.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtSenha.Name = "txtSenha";
-            this.txtSenha.Size = new System.Drawing.Size(309, 22);
+            this.txtSenha.Size = new System.Drawing.Size(233, 20);
             this.txtSenha.TabIndex = 5;
+            this.txtSenha.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(86, 125);
+            this.txtEmail.Location = new System.Drawing.Point(64, 102);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(309, 22);
+            this.txtEmail.Size = new System.Drawing.Size(233, 20);
             this.txtEmail.TabIndex = 4;
+            this.txtEmail.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(86, 75);
+            this.txtNome.Location = new System.Drawing.Point(64, 61);
+            this.txtNome.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(309, 22);
+            this.txtNome.Size = new System.Drawing.Size(233, 20);
             this.txtNome.TabIndex = 3;
+            this.txtNome.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // frmCadastro
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.pnlCadastro);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmCadastro";
-            this.Text = "frmCadastro";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro";
             this.pnlCadastro.ResumeLayout(false);
             this.pnlCadastro.PerformLayout();
             this.ResumeLayout(false);
--- PrimeiroProjeto/PrimeiroProjeto/frmPrincipal.Designer.cs
+++ PrimeiroProjeto/PrimeiroProjeto/frmPrincipal.Designer.cs
@@ -35,19 +35,22 @@
             // 
             this.lblBoasVindas.AutoSize = true;
             this.lblBoasVindas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblBoasVindas.Location = new System.Drawing.Point(47, 32);
+            this.lblBoasVindas.Location = new System.Drawing.Point(35, 26);
+            this.lblBoasVindas.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblBoasVindas.Name = "lblBoasVindas";
-            this.lblBoasVindas.Size = new System.Drawing.Size(0, 25);
+            this.lblBoasVindas.Size = new System.Drawing.Size(0, 20);
             this.lblBoasVindas.TabIndex = 0;
             // 
             // frmPrincipal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.lblBoasVindas);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmPrincipal";
-            this.Text = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Principal";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmPrincipal_FormClosing);
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmPrincipal_FormClosed);
             this.Load += new System.EventHandler(this.frmPrincipal_Load);
